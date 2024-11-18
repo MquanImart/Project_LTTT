856e0274869f118fdfcd57dbed32a177a7fdeb00
hung <hungat418@gmail.com>
2024-11-19 03:18:36 +0700

    info

--- src/features/profile/components/UserDetail.tsx
+++ src/features/profile/components/UserDetail.tsx
@@ -2,6 +2,8 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import Styles from "./Styles";
 import { useState } from "react";
 import Icon from "react-native-vector-icons/Ionicons";
+import { Button } from "react-native-paper";
+import { DatePickerModal } from "react-native-paper-dates";
 
 interface UserDetailProps {
     firstName: string;
@@ -35,9 +37,18 @@ const UserDetail = ({
     isEditable,
 }: UserDetailProps) => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [openDatePicker, setOpenDatePicker] = useState(false);
+
 
     const togglePasswordVisibility = () => {
         setIsPasswordVisible(!isPasswordVisible);
+    };
+
+    const handleDateConfirm = (date: Date | undefined) => {
+        if (date) {
+            setBirthDate(date.toISOString().split("T")[0]); // Format yyyy-MM-dd
+        }
+        setOpenDatePicker(false);
     };
 
     return (
@@ -66,19 +77,28 @@ const UserDetail = ({
             </View>
 
             <Text>Ngày sinh:</Text>
-            <TextInput
-                style={Styles.input}
-                value={birthDate}
-                onChangeText={setBirthDate}
-                editable={isEditable} 
-                placeholder="Ngày sinh"
+            <Button
+                mode="contained"
+                onPress={() => setOpenDatePicker(true)}
+                disabled={!isEditable}
+                contentStyle={{ backgroundColor: isEditable ? "#4CAF50" : "#ccc" }}
+            >
+                Ngày sinh: {birthDate || "Chưa chọn"}
+            </Button>
+            <DatePickerModal
+                locale="vi" // Tiếng Việt
+                mode="single"
+                visible={openDatePicker}
+                onDismiss={() => setOpenDatePicker(false)}
+                date={birthDate ? new Date(birthDate) : undefined} // Hiển thị ngày từ database
+                onConfirm={(params) => handleDateConfirm(params.date)}
+                validRange={{ endDate: new Date() }}
             />
 
             <Text>Số điện thoại:</Text>
             <TextInput
                 style={Styles.input}
                 value={phoneNumber}
-                onChangeText={setPhoneNumber}
                 editable={isEditable}
                 placeholder="Số điện thoại"
                 keyboardType="phone-pad"
@@ -130,6 +150,7 @@ const UserDetail = ({
                     placeholder="Mật khẩu"
                     value={password}
                     secureTextEntry={!isPasswordVisible}
+                    onChangeText={setPassword} 
                     editable={isEditable} 
                 />
                 <TouchableOpacity
